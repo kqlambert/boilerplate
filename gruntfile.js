47776aeed74d03c1b4d969958595e972b046f2b3
keegan lambert <kqlambert@gmail.com>
2017-02-05 11:07:58 -0700

    added imagemin

--- gruntfile.js
+++ gruntfile.js
@@ -47,6 +47,14 @@ module.exports = function (grunt) {
 				}
 			}
 		},
+		dynamic: {
+			files: [{
+				expand: true,
+				cwd: 'i/',
+				src: ['**/*.{png,jpg,gif}'],
+				dest: 'i/'
+			}]
+		},
 		watch: {
 			options: {
 				livereload: true
@@ -73,6 +81,7 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-autoprefixer');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.registerTask('default', ['concat', 'uglify', 'sass', 'autoprefixer', 'cssmin', 'watch']);
